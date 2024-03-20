12b48a331e19ecd271caa60d8bdf28193e7632ca
Developer (Laurens) <55593054+deveolper@users.noreply.github.com>
2024-03-20 14:38:07 +0100

    fix: meerdere visualisatieopties

--- datawarehouse/src/dashboard.py
+++ datawarehouse/src/dashboard.py
@@ -8,6 +8,7 @@ product = pd.read_sql_query("SELECT * FROM product", go_dwh.conns[1])
 
 
 def main():
+    import matplotlib.pyplot as plt
     print("Overzicht van alle werkelijke orders:")
     for row in order_details.iterrows():
         print(row)
@@ -16,8 +17,17 @@ def main():
     for row in go_dwh.returned_item.iterrows():
         print(row)
 
-    print("Hoeveel producten hebben we?")
-    print(len(product))
+    print("Wat zijn de productie-kosten van onze producten?")
+    product.sort_values(by="PRODUCTION_COST", inplace=True)
+    print(product[["PRODUCT_NAME", "PRODUCTION_COST"]])
+
+    plt.xlabel('Producten')
+    plt.ylabel('Prijs')
+    plt.title('Staafdiagram van Productprijzen')
+
+    plt.bar(product["PRODUCT_NAME"], product["PRODUCTION_COST"])
+
+    plt.show()
 
 
 if __name__ == "__main__":
