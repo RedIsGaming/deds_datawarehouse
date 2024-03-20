27a013d378d13974b4493d2a247b5f33e8e2634a
Developer (Laurens) <55593054+deveolper@users.noreply.github.com>
2024-03-20 14:22:11 +0100

    fix: bug

--- datawarehouse/src/dashboard.py
+++ datawarehouse/src/dashboard.py
@@ -4,7 +4,7 @@ import go_dwh
 
 
 order_details = pd.read_sql_query("SELECT * FROM order_details", go_dwh.conns[1])
-# go_dwh.returned_item
+product = pd.read_sql_query("SELECT * FROM product", go_dwh.conns[1])
 
 
 def main():
@@ -15,6 +15,9 @@ def main():
     print("Overzicht van alle teruggebrachte items:")
     for row in go_dwh.returned_item.iterrows():
         print(row)
+
+    print("Hoeveel producten hebben we?")
+    print(len(product))
 
 
 if __name__ == "__main__":
