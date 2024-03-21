4912e3cf26b5bba29712769ae509a5fc4983fa83
Developer (Laurens) <55593054+deveolper@users.noreply.github.com>
2024-03-21 15:48:00 +0100

    wip: stuff

--- datawarehouse/src/dashboard.py
+++ datawarehouse/src/dashboard.py
@@ -1,33 +1,138 @@
+import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 
 import go_dwh
+import pyodbc
+import proglog
 
 
 order_details = pd.read_sql_query("SELECT * FROM order_details", go_dwh.conns[1])
 product = pd.read_sql_query("SELECT * FROM product", go_dwh.conns[1])
+retailer = pd.read_sql_query("SELECT * FROM retailer", go_dwh.conns[0])
+
+def stuff():
+    product = pd.read_sql("SELECT * FROM product", go_dwh.conn_db2)
+    sales_staff = pd.read_sql("SELECT * FROM sales_staff", go_dwh.conn_db3)
+    return_reason = pd.read_sql("SELECT * FROM return_reason", go_dwh.conn_db2)
+    course = pd.read_sql("SELECT * FROM course", go_dwh.conn_db3)
+    product_type = pd.read_sql("SELECT * FROM product_type", go_dwh.conn_db2)
+    product_line = pd.read_sql("SELECT * FROM product_line", go_dwh.conn_db2)
+
+    # retailer inlezen
+
+    retailer_site = pd.read_sql("SELECT * FROM retailer_site", go_dwh.conn_db1)
+    retailer_contact = pd.read_sql("SELECT * FROM retailer_contact", go_dwh.conn_db1)
+    retailer_segment = pd.read_sql("SELECT * FROM retailer_segment", go_dwh.conn_db1)
+    retailer_type = pd.read_sql("SELECT * FROM retailer_type", go_dwh.conn_db1)
+    age_group = pd.read_sql("SELECT * FROM age_group", go_dwh.conn_db1)
+    retailer = pd.read_sql("SELECT * FROM retailer", go_dwh.conn_db1)
+    retailer_headquarters = pd.read_sql("SELECT * FROM retailer_headquarters", go_dwh.conn_db1)
+    sales_demographic = pd.read_sql("SELECT * FROM sales_demographic", go_dwh.conn_db1)
+    country = pd.read_sql("SELECT * FROM country", go_dwh.conn_db1)
+    sales_territory = pd.read_sql("SELECT * FROM sales_territory", go_dwh.conn_db1)
+
+    #retailer merging
+
+    retailer_site_contact = pd.merge(retailer_site, retailer_contact, on='RETAILER_SITE_CODE')
+    retailer_site_contact_retailer = pd.merge(retailer_site_contact, retailer, on='RETAILER_CODE')
+    retailer_site_contact_type = pd.merge(retailer_site_contact_retailer, retailer_type, on='RETAILER_TYPE_CODE')
+    segment_headquarters = pd.merge(retailer_segment, retailer_headquarters, on="SEGMENT_CODE")
+    retailer_site_contact_type_sh = pd.merge(segment_headquarters, retailer_site_contact_type, on="RETAILER_CODEMR")
+    age_sales_demo = pd.merge(age_group, sales_demographic, on="AGE_GROUP_CODE")
+    retailer_site_contact_type_sh_age = pd.merge(retailer_site_contact_type_sh, age_sales_demo, on="RETAILER_CODEMR")
+    retailer_site_contact_type_sh_age_country = pd.merge(retailer_site_contact_type_sh_age, country, left_on="COUNTRY_CODE_x", right_on="COUNTRY_CODE")
+    retailer_df = pd.merge(retailer_site_contact_type_sh_age_country, sales_territory, on="SALES_TERRITORY_CODE")
+
+    #product merging
+
+    product_type_line = pd.merge(product_line, product_type, on="PRODUCT_LINE_CODE")
+    product_df = pd.merge(product, product_type_line, on="PRODUCT_TYPE_CODE")
+
+    return retailer_df
 
 
 def main():
-    print("Overzicht van alle werkelijke orders:")
-    for row in order_details.iterrows():
-        print(row)
+    # print("Overzicht van alle werkelijke orders:")
+    # for row in order_details.iterrows():
+    #     print(row)
 
-    print("Overzicht van alle teruggebrachte items:")
-    for row in go_dwh.returned_item.iterrows():
-        print(row)
+    # print("Overzicht van alle teruggebrachte items:")
+    # for row in go_dwh.returned_item.iterrows():
+    #     print(row)
 
-    print("Wat zijn de productie-kosten van onze producten?")
-    product.sort_values(by="PRODUCTION_COST", inplace=True)
-    print(product[["PRODUCT_NAME", "PRODUCTION_COST"]])
+    # print("Wat zijn de productie-kosten van onze producten?")
+    # product.sort_values(by="PRODUCTION_COST", inplace=True)
+    # print(product[["PRODUCT_NAME", "PRODUCTION_COST"]])
 
-    plt.xlabel('Producten')
-    plt.ylabel('Prijs')
-    plt.title('Staafdiagram van productiekosten')
+    # plt.xlabel('Producten')
+    # plt.ylabel('Prijs')
+    # plt.title('Staafdiagram van productiekosten')
 
-    plt.bar(product["PRODUCT_NAME"], product["PRODUCTION_COST"])
+    # plt.bar(product["PRODUCT_NAME"], product["PRODUCTION_COST"])
 
-    plt.show()
+    # plt.show()
+
+    query = "INSERT INTO retailer VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    
+    retailer_site_contact_type_sh_age = stuff()
+
+    export_conn = pyodbc.connect(go_dwh.conn_str)
+    export_cursor = export_conn.cursor()
+
+    for index, row in retailer_site_contact_type_sh_age.iterrows():
+        values = (
+            row['RETAILER_SITE_CODE'],
+            row['RETAILER_CODEMR'],
+            row['COMPANY_NAME'],
+            row['POSTAL_ZONE_x'],
+            row['REGION_x'],
+            row['CITY_x'],
+            row['RETAILER_TYPE_EN'],
+            row['RETAILER_TYPE_CODE'],
+            row['COUNTRY_CODE_x'],
+            row['COUNTRY_EN'],
+            row['SEGMENT_CODE'],
+            row['SEGMENT_NAME'],
+            row['GENDER'],
+            row['AGE_GROUP_CODE'],
+            row['UPPER_AGE'],
+            row['LOWER_AGE'],
+            row['SALES_TERRITORY_CODE'],
+            row['TERRITORY_NAME_EN']
+        )         
+        export_cursor.execute(query, *values)
+
+    print(export_cursor.execute("SELECT * FROM retailer").fetchall())
+
+    product = pd.read_sql("SELECT * FROM product", go_dwh.conn_db2)
+
+    def format_date(date_str):
+        date_obj = datetime.datetime.strptime(date_str, '%d-%m-%Y')
+        formatted_date = date_obj.strftime('%Y-%m-%d')
+        return formatted_date
+
+    print(product.columns)
+    for _, row in product.iterrows():
+        sales_price = float(row['PRODUCTION_COST']) * (1 + float(row['MARGIN']))
+        formatted_date = format_date(row['INTRODUCTION_DATE'])
+        query = "INSERT INTO product VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)" 
+        values = (
+            row['PRODUCT_NAME'],
+            row['DESCRIPTION'],
+            sales_price,
+            row['LANGUAGE'],
+            row['PRODUCTION_COST'],
+            row['MARGIN'],
+            formatted_date,
+            row['PRODUCT_TYPE_CODE'],
+            "",
+            ""
+        )         
+        export_cursor.execute(query, *values)
+
+    export_conn.commit()
+    export_conn.close()
 
 
 if __name__ == "__main__":
--- datawarehouse/src/go_dwh.py
+++ datawarehouse/src/go_dwh.py
@@ -86,21 +86,21 @@ for cursor, _ in zip(cursors, conns):
 # Read the tables with pandas from SQL Server.
 
 product = pd.read_sql_query("SELECT * FROM product", conn)
-sales_product_forecast = pd.read_sql_query("SELECT * FROM sales_product_forecast", conn)
-sales_inventory_levels = pd.read_sql_query("SELECT * FROM sales_inventory_levels", conn)
+# sales_product_forecast = pd.read_sql_query("SELECT * FROM sales_product_forecast", conn)
+# sales_inventory_levels = pd.read_sql_query("SELECT * FROM sales_inventory_levels", conn)
 sales_staff = pd.read_sql_query("SELECT * FROM sales_staff", conn)
-sales_targetdata = pd.read_sql_query("SELECT * FROM sales_targetdata", conn)
+# sales_targetdata = pd.read_sql_query("SELECT * FROM sales_targetdata", conn)
 course = pd.read_sql_query("SELECT * FROM course", conn)
-training = pd.read_sql_query("SELECT * FROM training", conn)
+# training = pd.read_sql_query("SELECT * FROM training", conn)
 
-satisfaction_type = pd.read_sql_query("SELECT * FROM satisfaction_type", conn)
-satisfaction = pd.read_sql_query("SELECT * FROM satisfaction", conn)
-returned_reason = pd.read_sql_query("SELECT * FROM returned_reason", conn)
+# satisfaction_type = pd.read_sql_query("SELECT * FROM satisfaction_type", conn)
+# satisfaction = pd.read_sql_query("SELECT * FROM satisfaction", conn)
+# returned_reason = pd.read_sql_query("SELECT * FROM returned_reason", conn)
 returned_item = pd.read_sql_query("SELECT * FROM returned_item", conns[1])
-retailer_contact = pd.read_sql_query("SELECT * FROM retailer_contact", conn)
-order_method = pd.read_sql_query("SELECT * FROM order_method", conn)
+# retailer_contact = pd.read_sql_query("SELECT * FROM retailer_contact", conn)
+# order_method = pd.read_sql_query("SELECT * FROM order_method", conn)
 
-df = retailer_contact
+# df = retailer_contact
 
 import copy
 import datetime
@@ -108,6 +108,9 @@ import datetime
 # Creating a slowly changing dimension (assuming type 2) for every dimension in the datawarehouse
 for name in ["product", "sales_product_forecast", "sales_inventory_levels", "sales_staff", "sales_targetdata", "course", "training", "satisfaction_type", "satisfaction", "returned_reason", "returned_item", "retailer_contact", "order_method"]:
     # Copy
+    if name not in globals():
+        continue
+
     globals()[name + "_scd"] = copy.deepcopy(globals()[name])
 
     # Add columns
@@ -125,113 +128,113 @@ for name in ["product", "sales_product_forecast", "sales_inventory_levels", "sal
 
 # Merging source
 print(returned_item.columns)
-print(returned_reason.columns)
+# print(returned_reason.columns)
 
-source_1 = pd.merge(
-    returned_reason,
-    returned_item,
-    left_on="return_reason_code",
-    right_on="RETURN_REASON_CODE"
-)
+# source_1 = pd.merge(
+#     returned_reason,
+#     returned_item,
+#     left_on="return_reason_code",
+#     right_on="RETURN_REASON_CODE"
+# )
 
-source_2 = pd.merge(
-    satisfaction_type,
-    pd.merge(
-        satisfaction,
-        pd.merge(
-            course,
-            pd.merge(
-                training,
-                pd.merge(
-                    sales_staff,
-                    pd.merge(
-                        sales_inventory_levels,
-                        pd.merge(
-                            sales_targetdata,
-                            pd.merge(
-                                product,
-                                sales_product_forecast,
-                                on="product_number"
-                            ),
-                            left_on="target_product_nr",
-                            right_on="product_number"
-                        ),
-                        on="product_number"
-                    ),
-                    left_on="sales_staff_code",
-                    right_on="sales_staff_id"
-                ),
-                left_on="training_staff_id",
-                right_on="sales_staff_code"
-            ),
-            left_on="course_code",
-            right_on="training_course_id"
-        ),
-        left_on="satisfaction_staff_id",
-        right_on="sales_staff_code"
-    ),
-    left_on="satisfaction_type_code",
-    right_on="satisfaction_type_id"
-)
+# source_2 = pd.merge(
+#     satisfaction_type,
+#     pd.merge(
+#         satisfaction,
+#         pd.merge(
+#             course,
+#             pd.merge(
+#                 training,
+#                 pd.merge(
+#                     sales_staff,
+#                     pd.merge(
+#                         sales_inventory_levels,
+#                         pd.merge(
+#                             sales_targetdata,
+#                             pd.merge(
+#                                 product,
+#                                 sales_product_forecast,
+#                                 on="product_number"
+#                             ),
+#                             left_on="target_product_nr",
+#                             right_on="product_number"
+#                         ),
+#                         on="product_number"
+#                     ),
+#                     left_on="sales_staff_code",
+#                     right_on="sales_staff_id"
+#                 ),
+#                 left_on="training_staff_id",
+#                 right_on="sales_staff_code"
+#             ),
+#             left_on="course_code",
+#             right_on="training_course_id"
+#         ),
+#         left_on="satisfaction_staff_id",
+#         right_on="sales_staff_code"
+#     ),
+#     left_on="satisfaction_type_code",
+#     right_on="satisfaction_type_id"
+# )
 
-source_3 = retailer_contact
-source_4 = order_method
+# source_3 = retailer_contact
+# source_4 = order_method
 
 # Merging warehouse
-warehouse_1 = pd.merge(
-    returned_reason_scd,
-    returned_item_scd,
-    left_on="return_reason_code",
-    right_on="RETURN_REASON_CODE"
-)
+# warehouse_1 = pd.merge(
+#     returned_reason_scd,
+#     returned_item_scd,
+#     left_on="return_reason_code",
+#     right_on="RETURN_REASON_CODE"
+# )
 
-warehouse_2 = pd.merge(
-    satisfaction_type_scd,
-    pd.merge(
-        satisfaction_scd,
-        pd.merge(
-            course_scd,
-            pd.merge(
-                training_scd,
-                pd.merge(
-                    sales_staff_scd,
-                    pd.merge(
-                        sales_inventory_levels_scd,
-                        pd.merge(
-                            sales_targetdata_scd,
-                            pd.merge(
-                                product,
-                                sales_product_forecast_scd,
-                                on="product_number"
-                            ),
-                            left_on="target_product_nr",
-                            right_on="product_number"
-                        ),
-                        on="product_number"
-                    ),
-                    left_on="sales_staff_code",
-                    right_on="sales_staff_id",
-                    suffixes=("_1", "_2")
-                ),
-                left_on="training_staff_id",
-                right_on="sales_staff_code"
-            ),
-            left_on="course_code",
-            right_on="training_course_id",
-            suffixes=("_3", "_4")
-        ),
-        left_on="satisfaction_staff_id",
-        right_on="sales_staff_code",
-        suffixes=("_5", "_6")
-    ),
-    left_on="satisfaction_type_code",
-    right_on="satisfaction_type_id",
-    suffixes=("_7", "_8")
-)
+# warehouse_2 = pd.merge(
+#     satisfaction_type_scd,
+#     pd.merge(
+#         satisfaction_scd,
+#         pd.merge(
+#             course_scd,
+#             pd.merge(
+#                 training_scd,
+#                 pd.merge(
+#                     sales_staff_scd,
+#                     pd.merge(
+#                         sales_inventory_levels_scd,
+#                         pd.merge(
+#                             sales_targetdata_scd,
+#                             pd.merge(
+#                                 product,
+#                                 sales_product_forecast_scd,
+#                                 on="product_number"
+#                             ),
+#                             left_on="target_product_nr",
+#                             right_on="product_number"
+#                         ),
+#                         on="product_number"
+#                     ),
+#                     left_on="sales_staff_code",
+#                     right_on="sales_staff_id",
+#                     suffixes=("_1", "_2")
+#                 ),
+#                 left_on="training_staff_id",
+#                 right_on="sales_staff_code"
+#             ),
+#             left_on="course_code",
+#             right_on="training_course_id",
+#             suffixes=("_3", "_4")
+#         ),
+#         left_on="satisfaction_staff_id",
+#         right_on="sales_staff_code",
+#         suffixes=("_5", "_6")
+#     ),
+#     left_on="satisfaction_type_code",
+#     right_on="satisfaction_type_id",
+#     suffixes=("_7", "_8")
+# )
 
-warehouse_3 = retailer_contact_scd
-warehouse_4 = order_method_scd
+# warehouse_3 = retailer_contact_scd
+# warehouse_4 = order_method_scd
 
 # Asserting everything
-for source, warehouse in zip([source_1, source_2, source_3, source_4], [warehouse_1, warehouse_2, warehouse_3, warehouse_4]):
-    assert source.drop_duplicates().size == warehouse.drop_duplicates().size
+# for source, warehouse in zip([source_1, source_2, source_3, source_4], [warehouse_1, warehouse_2, warehouse_3, warehouse_4]):
+#     assert source.drop_duplicates().size == warehouse.drop_duplicates().size
